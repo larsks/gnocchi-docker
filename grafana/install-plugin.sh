38fa4a065c9d5de3655c6d3b7f52d7228107736b
Lars Kellogg-Stedman <lars@redhat.com>
2017-08-17 16:08:42 -0400

    allow git repo and branch to be specified in env

--- grafana/install-plugin.sh
+++ grafana/install-plugin.sh
@@ -4,11 +4,13 @@ set -e
 set -x
 
 : "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
+: "${GF_PLUGIN_REPO:=https://github.com/gnocchixyz/grafana-gnocchi-datasource}"
+: "${GF_PLUGIN_BRANCH:=master}"
+
 mkdir -p ${GF_PATHS_PLUGINS}
 cd ${GF_PATHS_PLUGINS}
 
 # ensure we have the latest plugin
 rm -rf gnocchixyz-gnocchi-datasource
-git clone -b feature/names \
-	https://github.com/larsks/grafana-gnocchi-datasource \
+git clone -b ${GF_PLUGIN_BRANCH} ${GF_PLUGIN_REPO} \
 	gnocchixyz-gnocchi-datasource
